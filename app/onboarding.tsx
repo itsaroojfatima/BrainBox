3fa560ebcd234e00664ffe24afb4d45598b68338
Arooj Fatima <bc250430386@vu.edu.pk>
2026-04-21 20:51:24 +0500

    Create onboarding.tsx

---
+++ app/onboarding.tsx
@@ -0,0 +1,96 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Onboarding() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.title}>Skip</Text>
+      </TouchableOpacity>
+      <Image
+        source={require("@/assets/images/robot.png")}
+        style={styles.image}
+      />
+      <View style={styles.dots}>
+        <View style={[styles.dot, styles.activeDot]} />
+        <View style={styles.dot} />
+        <View style={styles.dot} />
+      </View>
+      <Text style={styles.subtitle}>Unlock the Power of Future AI</Text>
+
+      <Text style={styles.heading}>
+        Chat with the smartest AI. Experience power of AI with us
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/onboardering2")}
+      >
+        <AntDesign name="arrow-right" size={24} color="#fff" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  image: {
+    width: 250,
+    height: 250,
+  },
+
+  dots: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#ccc",
+    marginHorizontal: 5,
+  },
+
+  activeDot: {
+    backgroundColor: "#141718",
+  },
+
+  title: {
+    top: 50,
+    right: 20,
+    fontSize: 14,
+    color: "gray",
+  },
+
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 20,
+    textAlign: "center",
+  },
+
+  heading: {
+    textAlign: "center",
+    color: "gray",
+    marginTop: 10,
+  },
+
+  button: {
+    backgroundColor: "#FF7A1A",
+    width: "90%",
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 30,
+    alignItems: "center",
+  },
+});
