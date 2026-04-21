f52ec135054b1cddc70a0430fbfd6a31b55882a9
Muhammad Usman Tariq <usmanjutt04747@gmail.com>
2026-04-21 21:11:57 +0500

    Revert "app created"
    This reverts commit f1e4f401878876b98e44c38f156b666a3bb71688.

---
+++ app/index.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/onboardering");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/images/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>BrainBox</Text>
+      <Text style={styles.subtitle}>Version 10</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 119.02,
+    height: 142.49,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 25,
+    color: "000000",
+  },
+
+  subtitle: {
+    textAlign: "center",
+    color: "gray",
+    marginTop: 10,
+  },
+});
